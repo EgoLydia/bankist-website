8d128aaec1c68858901bdf4f7eed0bb4b2c81813
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-07-12 12:02:41 +0100

    implement tabbed components

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const navLinks = document.querySelector('.nav__links');
+const tabs = document.querySelectorAll('operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -60,6 +63,23 @@ navLinks.addEventListener('click', function (e) {
     //select the href
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
+});
+
+//implement tab component
+tabContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  //guard clause
+  if (!clicked) return;
+
+  //remove active class in tabs and content area
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  //activate tabs and content area
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
 
 // const h1 = document.querySelector('h1');
